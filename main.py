1882b9673e9c90ea424dd6532c96ded0aa86c9f0
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-11-26 20:45:13 +0300

    Start

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def start(name):
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    start('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
