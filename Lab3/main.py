a12b95934f58204ba23c965f01bc13244edbb436
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2020-12-05 19:56:57 +0300

    Lab 5

--- Lab3/main.py
+++ Lab3/main.py
@@ -27,8 +27,6 @@ def start():
             recList.append([row, name, value])
             tempMat = tempMat.drop([tempMat.idxmax().item()], axis=0)
             k += 1
-    recomendations = recomendations.append(recList, ignore_index=True)
-    recomendations.to_excel('result.xlsx')
 
     output = list()
     for element in recList:
--- Lab5/main.py
+++ Lab5/main.py
@@ -2,15 +2,54 @@ import pandas as pd
 
 
 def start():
+    recomendations = pd.DataFrame()
     df = pd.read_excel('Book.xlsx')
     df = df.astype(str)
     df = df.set_index('Тема').T
     df.to_excel('DataSet.xlsx')
-    df = pd.read_excel('DataSet.xlsx', index_col=0)
-    recList = list()
 
-    print('Введите назание понятия:')
-    text = input()
+    recList = []
+
+    print('Введите год принятия закона:')
+    Year = int(input())
+    print('Введите колличество аргуменов:')
+    Arg = int(input())
+    print('Введите степень точности, те 10^(-9) => 9:')
+    Accuracy = int(input())
+    print('Введите уровень на дереве:')
+    Level = input()
+    print('Популярный? Введите да/нет:')
+    popular = input()
+    print('Введите назание закона:')
+    Law = input()
+    result = []
+    inter = [result]
+
+    result.append(Year)
+    result.append(Arg)
+    result.append(Accuracy)
+    result.append(Level)
+    result.append(popular)
+    if result[4] == 'да':
+        result[4] = 1
+    elif result[4] == 'нет':
+        result[4] = 0
+    else:
+        result[4] = 0
+    result.append(Law)
+
+    df = pd.read_excel('Book.xlsx', index_col=0)
+    col = pd.DataFrame(inter, index=['Yours'],
+                       columns=['Век', 'Аргументы', 'Точность', 'Уровень', 'Популярность', 'Закон'])
+    df = df.append(col)
+    df.to_excel('result.xlsx')
+
+    df = pd.read_excel('result.xlsx')
+    df = df.astype(str)
+    df = df.set_index('Тема').T
+    df.to_excel('DataSet.xlsx')
+    df = pd.read_excel('DataSet.xlsx', index_col=0)
+
 
     for row in df:
         k = 0
@@ -24,11 +63,13 @@ def start():
             recList.append([row, name, value])
             tempMat = tempMat.drop([tempMat.idxmax().item()], axis=0)
             k += 1
+    recomendations = recomendations.append(recList, ignore_index=True)
+    recomendations.to_excel('resultref.xlsx')
 
     output = list()
-    for element in recList:
-        if text == element[0]:
-            output.append(element)
+    # for element in recList:
+    #     if text == element[0]:
+    #         output.append(element)
 
     print('Возможно вам было бы интересны такие темы как:')
     counter = 0
